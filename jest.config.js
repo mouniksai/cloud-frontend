6e144e164241e436c5237a623d4725bf156e5c3e
Sanjjiiev S <sanjjiiev005@gmail.com>
2026-03-06 11:20:04 +0530

    featL updated testing files and ci pipeline

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files
+    dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterSetup: ['<rootDir>/jest.setup.js'],
+    testMatch: ['**/testing/**/*.test.{js,jsx}'],
+    collectCoverageFrom: [
+        'app/**/*.{js,jsx}',
+        'middleware.js',
+        '!app/layout.js',
+        '!app/globals.css',
+        '!**/node_modules/**',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 10,
+            functions: 10,
+            lines: 10,
+            statements: 10,
+        },
+    },
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+    verbose: true,
+};
+
+module.exports = createJestConfig(customJestConfig);
